c305a0586715c78dc31b027443d4421bbe3d876c
vishalpatel91912 <vishalpatel91912@gmail.com>
2023-10-20 20:23:33 +0100

    `java week01 hw

---
+++ src/java_programme_week_1/Programme_3_PrintIninitials.java
@@ -0,0 +1,21 @@
+package java_programme_week_1;
+
+/**
+ * 3. Write a program that will print your initials to standard output in letters that
+ * are nine lines tall. Each big letter should be made up of a bunch of *'s. For
+ * example, if your initials were "JAJA", then the output would look something
+ * like:
+ */
+public class Programme_3_PrintIninitials {
+    public static void main(String[] args) {
+        System.out.println("*********  ********  *********  ********");
+        System.out.println("      **   **    **        **   **    **");
+        System.out.println("      **   **    **        **   **    **");
+        System.out.println("      **   **    **        **   **    **");
+        System.out.println("      **   ********        **   ********");
+        System.out.println("  **  **   **    **    **  **   **    **");
+        System.out.println("  **  **   **    **    **  **   **    **");
+        System.out.println("   ****    **    **     ****    **    **");
+        System.out.println("    **     **    **      **     **    **");
+    }
+}
---
+++ src/java_programme_week_1/Programme_4_RightTirangle.java
@@ -0,0 +1,23 @@
+package java_programme_week_1;
+
+/**
+ * 4. Write a Java Program to print below triangle
+ *
+ * *
+ * **
+ * ***
+ * ****
+ * *****
+ * ******
+ */
+public class Programme_4_RightTirangle {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+        System.out.println("******");
+    }
+
+}
